579ebf948265edc4e0bc7184e074bf56a0695b2f
jedrekszor <jedrekszor@wp.pl>
2019-01-25 21:47:46 +0100

    flag

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (transform.position.y < 5)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime);
+            }
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            if (transform.position.y > 0)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime);
+            }
+        }
+    }
+}
