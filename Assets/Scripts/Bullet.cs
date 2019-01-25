2769c1f4ecc242687506f709783bfb23f6f12b08
jedrekszor <jedrekszor@wp.pl>
2019-01-26 00:21:46 +0100

    shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * vel;
+        StartCoroutine(destroy());
+    }
+
+    IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(4);
+        Destroy(gameObject);
+    }
+}
