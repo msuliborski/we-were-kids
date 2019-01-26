ca44f4af7b9c2ecd75228738948ca717e9ca64be
Michał Suliborski <msuliborski@gmail.com>
2019-01-26 14:36:58 +0100

    ladny commit message

---
+++ Assets/Scripts/Weapons/PickUpAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpAnim : MonoBehaviour
+{
+    [SerializeField] private float rot = 2;
+    [SerializeField] private float scl = 0.01f;
+    [SerializeField] private float max = 1.4f;
+    [SerializeField] private float min = 0.6f;
+
+    private bool grow = false;
+    // Start is called before the first frame update
+//    void Start()
+//    {
+//        GetComponent<Rigidbody>().angularVelocity = Vector3.up * rot;
+//    }
+//
+//    // Update is called once per frame
+//    void FixedUpdate()
+//    {
+//        if (transform.localScale.x > max)
+//            grow = true;
+//        else if (transform.localScale.x < min)
+//            grow = false;
+//        
+//        
+//        if (!grow)
+//        {
+//            transform.localScale = new Vector3(transform.localScale.x+scl, transform.localScale.y+scl,transform.localScale.z+scl);
+//        }
+//        else
+//        {
+//            transform.localScale = new Vector3(transform.localScale.x-scl, transform.localScale.y-scl,transform.localScale.z-scl);
+//        }
+//    }
+}
---
+++ Assets/Scripts/Weapons/spawnHere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnHere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject objectToSpawn;
+    public float rate = 5;
+    private float counting;
+    
+    void Start() {
+        counting = 2;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        counting -= Time.deltaTime;
+        if (counting < 0) counting = 0f;     
+        
+        if (Math.Abs(counting)*1f < 0.01) {
+            Instantiate(objectToSpawn, transform.position, Quaternion.Euler(0, 0, 0), transform);
+            counting = rate;
+        }
+
+        if (transform.childCount > 1) {
+            Destroy(transform.GetChild(1).gameObject);
+        }
+    }
+}
