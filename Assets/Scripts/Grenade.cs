a80d8cbd19f5eab2f4fe59ef6df3da248052b90a
jedrekszor <jedrekszor@wp.pl>
2019-01-26 09:45:09 +0100

    duke nukem box

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gr : MonoBehaviour
+{
+    [SerializeField] private GameObject grenade;
+    [SerializeField] private float delay;
+    [SerializeField] private GameObject player;
+    private bool picked = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!picked && (transform.position - player.transform.position).magnitude < 1)
+        {
+            picked = true;
+        }
+
+        if (picked)
+        {
+            transform.position = player.transform.position;
+        }
+    }
+
+    IEnumerator boom()
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+}
