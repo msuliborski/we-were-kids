f5c0d417cba18b48f1dee912d6130aed8b6c95fb
Michal Suliborski <msuliborski@gmail.com>
2019-01-26 03:51:24 +0000

    camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    
+    //[SerializeField] public int id;
+
+    [SerializeField] public GameObject player;
+    private Vector3 offset; 
+    void Start()
+    {
+        //player = GameObject.Find("Player"+id);
+        
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.position = player.transform.position + offset;
+    }
+}
