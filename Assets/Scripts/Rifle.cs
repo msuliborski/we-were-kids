b831f4d0b367775c51d2d5af9b5a3930bc1e2d79
Michal Kusmidrowicz <michal.kusmidrowicz@gmail.com>
2019-01-26 09:42:47 +0100

    ;k

--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rifle : MonoBehaviour
+public class Rifle : PickUp
 {
     // Start is called before the first frame update
     void Start()
