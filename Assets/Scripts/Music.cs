89a1259780e335bec221cf8525671f7459fa30bd
jedrekszor <jedrekszor@wp.pl>
2019-01-26 11:57:29 +0100

    music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField] AudioSource source1;
+    [SerializeField] AudioSource source2;
+    [SerializeField] AudioClip music;
+    [SerializeField] AudioClip battle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source1.clip = music;
+        source2.clip = battle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
