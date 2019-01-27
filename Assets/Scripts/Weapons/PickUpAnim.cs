82f2bf295472484cd51a874cee301d1d4537af31
jedrekszor <jedrekszor@wp.pl>
2019-01-27 14:04:03 +0100

    spinning

--- Assets/Scripts/Weapons/PickUpAnim.cs
+++ Assets/Scripts/Weapons/PickUpAnim.cs
@@ -6,36 +6,18 @@ using UnityEngine;
 public class PickUpAnim : MonoBehaviour
 {
     [SerializeField] private float ang = 1f;
-    [SerializeField] private float scl = 0.01f;
-    [SerializeField] private float max = 1.4f;
-    [SerializeField] private float min = 0.6f;
-    public PickUp owned;
-    private bool grow = false;
+    public PickUp owned;    
 
     void Start()
     {
-//        owned = GetComponent<PickUp>();
+        owned = GetComponent<PickUp>();
     }
     
     void FixedUpdate()
     {
-//        if (owned.owner == null)
-//        {
-//            if (transform.localScale.x > max)
-//                grow = true;
-//            else if (transform.localScale.x < min)
-//                grow = false;
-//        
-//            transform.Rotate(transform.up, ang);
-//        
-//            if (!grow)
-//            {
-//                transform.localScale = new Vector3(transform.localScale.x+scl, transform.localScale.y+scl,transform.localScale.z+scl);
-//            }
-//            else
-//            {
-//                transform.localScale = new Vector3(transform.localScale.x-scl, transform.localScale.y-scl,transform.localScale.z-scl);
-//            }    
-//        }
+        if (owned.owner == null)
+        {
+            transform.Rotate(transform.up, ang);    
+        }
     }
 }
