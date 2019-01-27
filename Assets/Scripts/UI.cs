d59f59133ede6d8beefa930fe8defb545c2ab25a
jedrekszor <jedrekszor@wp.pl>
2019-01-27 11:24:15 +0100

    ui

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<Sprite> sprites;
+    private Image p0;
+    private Image p1;
+    private PlayerHandler player0;
+    private PlayerHandler player1;
+    
     void Start()
     {
-        
+        player0 = GameObject.Find("Player0").GetComponent<PlayerHandler>();
+        player1 = GameObject.Find("Player1").GetComponent<PlayerHandler>();
+        p0 = transform.GetChild(0).GetComponent<Image>();
+        p1 = transform.GetChild(1).GetComponent<Image>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (player0.hp == 0)
+            p0.enabled = false;
+        else
+            p0.enabled = true;
         
+        if (player1.hp == 0)
+            p1.enabled = false;
+        else
+            p1.enabled = true;
+        
+        p0.sprite = sprites[player0.hp];
+        p1.sprite = sprites[player1.hp];
     }
 }
